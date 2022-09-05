e58d62c8e038ff23c995bb34c26255b61f3d9118
Sergio vasquez <103544569+Sergiotrader29@users.noreply.github.com>
2022-09-05 13:38:38 -0400

    Conectando Web Autores a base de datos SqlServer

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webApi.controllers.Entidades;
+
+namespace webapi
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AutorBase> Autores { get; set; }
+    }
+}
---
+++ Controllers/Entidades/AutorBase.cs
@@ -0,0 +1,8 @@
+﻿namespace webApi.controllers.Entidades
+{
+    public class AutorBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
