21c3f166de8f2a2bec82a0462bf33e5deb23354a
Sergio vasquez <103544569+Sergiotrader29@users.noreply.github.com>
2022-09-06 13:02:30 -0400

    Create Get post to DataBases

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webApi.controllers.Entidades;
+using webapi;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiAutores.controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AutorBase>>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(AutorBase autor )
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+
+}
