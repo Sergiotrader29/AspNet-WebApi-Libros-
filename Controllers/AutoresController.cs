5bc0600562024bf6ded91b1310a01570abb4ca1b
Sergio vasquez <103544569+Sergiotrader29@users.noreply.github.com>
2022-09-09 00:09:03 -0400

    Add HttpDelete and HttpPush

--- Controllers/AutoresController.cs
+++ Controllers/AutoresController.cs
@@ -29,6 +29,42 @@ namespace WebApiAutores.controllers
             await context.SaveChangesAsync();
             return Ok();
         }
+
+        [HttpPut("{id:int}")] // api/autores/algo
+        public async    Task<ActionResult> Put(AutorBase autor, int id)
+        {
+            if (autor.Id != id)
+            {
+                return BadRequest("El id del autor no coincide con el id de la Url"); //badrequest es un error 400
+            }
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id); //busca en la lista y anysync significa en algun lado
+
+            if (!existe)
+            {
+                return NotFound();   
+
+            }
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")] // api/autores/algo
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe =  await context.Autores.AnyAsync(x => x.Id == id); 
+
+            if (!existe)
+            {
+                return NotFound();
+
+            }
+
+            context.Remove(new AutorBase() { Id = id }); 
+            await context.SaveChangesAsync();
+            return Ok();
+        }
     }
 
 }
