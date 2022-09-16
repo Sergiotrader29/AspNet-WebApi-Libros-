7dedbf6bd202b8674a066a5815e7460bbcf7296d
Sergio vasquez <103544569+Sergiotrader29@users.noreply.github.com>
2022-09-16 12:56:54 -0400

    Create Validations.

--- Controllers/Entidades/AutorBase.cs
+++ Controllers/Entidades/AutorBase.cs
@@ -1,10 +1,16 @@
-﻿using webapi.Controllers.Entidades;
+﻿using System.ComponentModel.DataAnnotations;
+using webapi.Controllers.Entidades;
+using webapi.Validation;
 
 namespace webApi.controllers.Entidades
 {
     public class AutorBase
     {
         public int Id { get; set; }
+
+        [Required (ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 20, ErrorMessage = "El campo {0} no debe tener más de {1} carácteres")]
+        [FirstLetterMayusAtribute]
         public string Name { get; set; }
         public List<Libro> Libros { get; set; }
     }
---
+++ Validation/FirstLetterMayusAtribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapi.Validation
+{
+    public class FirstLetterMayusAtribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString())) // Con esto hago que no se solape dos validaciones.
+            {
+                return ValidationResult.Success; // si es nulo, con esto lo dejo pasar y que sea otra regla de REQUIRED que actue
+            }
+
+            var primeraLetra = value.ToString()[0].ToString();
+
+            if (primeraLetra != primeraLetra.ToUpper()) // si la primera letra no es mayuscula, retorne un error
+            {
+                return new ValidationResult("la primera letra debe ser mayúscula");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
