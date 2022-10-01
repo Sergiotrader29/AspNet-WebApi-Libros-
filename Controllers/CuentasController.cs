099edc467e353311a5e3cca53331e5e0eb6a7511
Sergio vasquez <103544569+Sergiotrader29@users.noreply.github.com>
2022-10-01 12:33:54 -0400

    Add Security CuentasController

---
+++ Controllers/CuentasController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Web.Helpers;
+using webapi.DTOs;
+
+namespace webapi.Controllers
+{
+
+    [ApiController]
+    [Route("api/cuentas")]
+
+    public class CuentasController
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IConfiguration configuration;
+
+        public CuentasController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("registrar")] //api/cuentas/registrar
+        public async Task<ActionResult<RespuestaAutenticacion>> Registrar(CredencialesUsuario credencialesUsuario)
+        {
+            var usuario = new IdentityUser
+            {
+                UserName = credencialesUsuario.Email,
+                Email = credencialesUsuario.Email
+            };
+
+            var resultado = await userManager.CreateAsync(usuario, credencialesUsuario.Password);
+
+            if(resultado.Succeeded)
+            {
+                return ConstruirToken(credencialesUsuario);
+            }
+            else
+            {
+                return BadRequest(resultado.Errors);
+            }
+        }
+    }
+
+    private RespuestaAutenticacion ConstruirToken(CredencialesUsuario credencialesUsuario)
+    {
+        var claims = new List<Claim>() // es informacion acerca del usuario confiable
+        {
+            new Claim("email", credencialesUsuario.Email) // el campo email va a tener un valor asignado que es credenciales
+
+        };
+
+        var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["LLavejwt"]));
+        var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
+
+        var expiracion = DateTime.UtcNow.AddYears(1);
+
+        var securityToken = new JwtSecurityToken(issuer: null, audience: null, claims: claims,
+            expires: expiracion, signingCredentials: creds);
+
+        return new RespuestaAutenticacion()
+        {
+            Token = new JwtSecurityTokenHandler().WriteToken(securityToken),
+            Expiracion = expiracion
+        };
+    }
+
+ }
